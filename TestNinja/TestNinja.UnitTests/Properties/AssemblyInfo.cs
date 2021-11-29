e45174f27bb1bf020ff6a335c7d46b67d8eddb95
Michal Och <ochrobot1990@gmail.com>
2021-11-29 16:47:27 +0100

    add test project and couple simple tests

---
+++ TestNinja/TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DXC Technology")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © DXC Technology 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86e87910-745e-4ee4-81ae-4ccdc4bc2ca9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
