e1fcb6bdd2ac5ff68c2c601f3b463224bf72d320
Michal Och <ochrobot1990@gmail.com>
2021-11-30 14:19:22 +0100

    add ignoring test example

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -20,6 +20,7 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
+        [Ignore("Because I wanted to")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             // Act 
