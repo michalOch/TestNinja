98486ea5c5bb80f57b24583d4e02fdb9aae99e1b
Michal Och <ochrobot1990@gmail.com>
2021-11-30 13:43:29 +0100

    add MathTests project and add test for add and max methods

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            // Arrange        
+            var math = new Math();
+            int a = 1, b = 2;
+
+            // Act 
+            var result = math.Add(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(a+b));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            // Arrange
+            int a = 2, b = 1;
+            var math = new Math();
+
+            // Act
+            var result = math.Max(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(a));
+        }        
+        
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            // Arrange
+            int a = 1, b = 2;
+            var math = new Math();
+
+            // Act
+            var result = math.Max(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            // Arrange
+            int a = 1, b = 1;
+            var math = new Math();
+
+            // Act
+            var result = math.Max(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(a));
+        }
+    }
+}
