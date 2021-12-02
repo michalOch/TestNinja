8432452e72aa4974defffe8e64ce87452b6bedfb
Michal Och <ochrobot1990@gmail.com>
2021-12-02 10:15:29 +0100

    add interaction test usung moq Verify method to check invocation of the mock at least once

---
+++ TestNinja/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_ShouldStoreTheOrder()
+        {
+            // Arrange
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            // Act
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            // Assert
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
