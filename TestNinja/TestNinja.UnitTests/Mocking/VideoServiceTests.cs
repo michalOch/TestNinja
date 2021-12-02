d0c6ea84509c35fc31c83043eafcd9639a64b8b4
Michal Och <ochrobot1990@gmail.com>
2021-12-02 09:04:37 +0100

    inject dependency as a method parameter

---
+++ TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            var expected = "Error parsing the video.";
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
