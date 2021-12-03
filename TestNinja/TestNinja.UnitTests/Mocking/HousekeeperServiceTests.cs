a939528950f4eed828b23de349db31d03cff69f1
Michal Och <ochrobot1990@gmail.com>
2021-12-03 12:41:10 +0100

    add test to check if SendStatementEmails run SaveStatement method from StatementGenerator class

--- TestNinja/TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -23,5 +25,40 @@ namespace TestNinja.UnitTests.Mocking
         // then use DI to inject those dependencie into HousekeeperHelper class
 
         // need to create some of interaction test to check if house keeper helper object talks with other objects like statementGenerator, emailSender properly
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Query<Housekeeper>()).Returns(new List<Housekeeper>
+            {
+                new Housekeeper
+                {
+                    Email = "a",
+                    FullName = "b",
+                    Oid = 1,
+                    StatementEmailBody = "c"
+                }
+            }.AsQueryable());
+
+            var statementGenerator = new Mock<IStatementGenerator>();
+            var emailSender = new Mock<IEmailSender>();
+            var messageBox = new Mock<IXtraMessageBox>();
+
+            var service = new HousekeeperService(
+                unitOfWork.Object, 
+                statementGenerator.Object, 
+                emailSender.Object, 
+                messageBox.Object);
+
+            service.SendStatementEmails(new DateTime(2017, 1, 1));
+
+            statementGenerator.Verify(sg => sg.SaveStatement(1, "b", new DateTime(2017, 1, 1)));
+        }
     }
 }
