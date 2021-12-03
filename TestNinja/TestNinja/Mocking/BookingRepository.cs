c917024ffefbc7f61cdc9fa4c0503865c4905eeb
Michal Och <ochrobot1990@gmail.com>
2021-12-03 09:53:19 +0100

    refactor BookingHelper -extract BookingRepository and IBookingRepository interface and inject it into  OverlappingBookingsExist via method parameter

---
+++ TestNinja/TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
+        {
+            var unitOfWork = new UnitOfWork();
+
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(b => b.Status != "Cancelled");
+
+            if (excludedBookingId.HasValue)
+                bookings = bookings.Where(b => b.Id != excludedBookingId.Value);
+
+            return bookings;
+        }
+    }
+}
